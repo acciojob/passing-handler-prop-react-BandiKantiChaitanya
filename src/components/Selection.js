e2053b309a697acc498dc35f26c2c30084dd6eac
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-22 16:43:55 +0530

    updated version

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -6,13 +6,23 @@ function Selection(props) {
     const { applyColor } = props;
     const [bgColor, setBgColor] = useState('');
 
+    const getTestId = (color) => {
+        switch (color) {
+          case 'rgb(34, 193, 195)': return 'blue';
+          case 'rgb(221, 112, 18)': return 'orange';
+          case 'rgb(44, 209, 88)': return 'green';
+          default: return '';
+        }
+      };
+      
+
     const handleClick = () => {
       applyColor((newColor) => {
         setBgColor(newColor.background);
       });
     };
     return (
-        <div className="fix-box" style={{ backgroundColor: bgColor }} onClick={handleClick}>
+        <div className="fix-box" style={{ backgroundColor: bgColor }} onClick={handleClick} data-testid={getTestId(bgColor)} >
           Selection
         </div>
       );
