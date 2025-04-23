064fbfc42ba4cdfe55c657a88b97e4b11507cf55
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-23 15:45:21 +0530

    Updated Version

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -2,10 +2,23 @@ import React from 'react';
 import '../styles/Child.css'
 
 const ColourSelector = (props) => {
+  
   const { config, selectNextBackground } = props
   const { background } = config;
+
+  // const getTestId = (color) => {
+  //   const normalized = color.replace(/\s/g, '');
+  //   switch (normalized) {
+  //     case 'rgb(34,193,195)': return 'blue';
+  //     case 'rgb(221,112,18)': return 'orange';
+  //     case 'rgb(44,209,88)': return 'green';
+  //     default: return '';
+  //   }
+  // };
+  // const testId = getTestId(bgColor);
   return (
-    <button className='selectorBtn'style={{backgroundColor:config.background}} onClick={() => selectNextBackground({background: background})}>
+    <button className='selectorBtn'style={{backgroundColor:config.background}} onClick={() => selectNextBackground({background: background})} data-testid={config.key} >
+      
       {config.label}
     </button>
   )
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -4,15 +4,7 @@ import '../styles/Child.css';
 function Selection({ applyColor }) {
   const [bgColor, setBgColor] = useState('');
 
-  const getTestId = (color) => {
-    const normalized = color.replace(/\s/g, '');
-    switch (normalized) {
-      case 'rgb(34,193,195)': return 'blue';
-      case 'rgb(221,112,18)': return 'orange';
-      case 'rgb(44,209,88)': return 'green';
-      default: return '';
-    }
-  };
+ 
 
   const handleClick = () => {
     applyColor((newColor) => {
@@ -20,14 +12,14 @@ function Selection({ applyColor }) {
     });
   };
 
-  const testId = getTestId(bgColor);
+  
 
   return (
     <div
       className="fix-box"
       style={{ backgroundColor: bgColor }}
       onClick={handleClick}
-      data-testid={testId}
+      
     >
       Selection
     </div>
